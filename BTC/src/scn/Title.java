9c463e43f4249ac30e5fcc572734d6205023b823
Huw Taylor <huw_taylor@hotmail.co.uk>
2013-11-17 21:37:55 +0000

    added exiting and misc details to basic placeholder title screen.

--- BTC/src/scn/Title.java
+++ BTC/src/scn/Title.java
@@ -1,8 +1,7 @@
 package scn;
 
-import java.util.Calendar;
-
 import lib.jog.graphics;
+import lib.jog.input;
 import lib.jog.window;
 import btc.Main;
 
@@ -30,8 +29,15 @@ public class Title extends Scene {
 	}
 
 	@Override
-	public void mouseReleased(int key, int x, int y) {
-		
+	public void mouseReleased(int key, int mx, int my) {
+		int x, y, w, h;
+		x = window.height();
+		y = window.height()/2 + 152;
+		w = window.width() - window.height();
+		h = 28;
+		if (mx >= x && mx <= x + w && my >= y && my <= y + h) {
+			_main.quit();
+		}
 	}
 
 	@Override
@@ -70,10 +76,7 @@ public class Title extends Scene {
 		graphics.arc(true, window.height()/2, window.height()/2, window.height()/2 - 32, angle, -1 * Math.PI / 8);
 		
 		// Title
-		double distance = Math.PI / 32;
-		int radius = 128;
 		String title = "Bear Traffic Controller";
-
 		double a = angle + (Math.PI * 4 / 5);
 		for (int i = 0; i < title.length(); i++) {
 			a -= Math.PI / 32;
@@ -94,6 +97,52 @@ public class Title extends Scene {
 		graphics.print(dateFormat.format(date), window.height() + 8, 20);
 		graphics.print(timeFormat.format(date), window.height() + 8, 36);
 		graphics.line(window.height(), 48, window.width() - 16, 48);
+		
+		graphics.line(window.height(), 108, window.width() - 16, 108);
+		graphics.print("Statistics:", window.height() + 8, 116);
+		graphics.line(window.height(), 130, window.width() - 16, 130);
+		graphics.print("Time Played:", window.height() + 8, 138);
+		int hours = (int)(_timer / (60 * 60));
+		int minutes = (int)(_timer / 60);
+		minutes %= 60;
+		double seconds = _timer % 60;
+		java.text.DecimalFormat df = new java.text.DecimalFormat("00.00");
+		String timePlayed = String.format("%d:%02d:", hours, minutes) + df.format(seconds); 
+		graphics.print(timePlayed, window.height() + 8, 154);
+		graphics.print("Current Level:", window.height() + 8, 170);
+		graphics.print("0", window.height() + 8, 186);
+		graphics.print("Current EXP:", window.height() + 8, 202);
+		graphics.print("0", window.height() + 8, 218);
+		
+		// Buttons
+		graphics.line(window.height(), window.height()/2 + 90, window.width() - 16, window.height()/2 + 90);
+		if (input.isMouseInRect(window.height(), window.height()/2 + 92, window.width() - window.height(), 28)) {
+			graphics.setColour(128, 128, 128);
+		} else {
+			graphics.setColour(0, 128, 0);
+		}
+		graphics.print("Play Demo", window.height() + 8, window.height()/2 + 102);
+		graphics.setColour(0, 128, 0);
+		graphics.line(window.height(), window.height()/2 + 120, window.width() - 16, window.height()/2 + 120);
+		if (input.isMouseInRect(window.height(), window.height()/2 + 122, window.width() - window.height(), 28)) {
+			graphics.setColour(128, 128, 128);
+		} else {
+			graphics.setColour(0, 128, 0);
+		}
+		graphics.print("Play Full Game", window.height() + 8, window.height()/2 + 132);
+		graphics.setColour(0, 128, 0);
+		graphics.line(window.height(), window.height()/2 + 150, window.width() - 16, window.height()/2 + 150);
+		if (input.isMouseInRect(window.height(), window.height()/2 + 152, window.width() - window.height(), 28)) {
+			graphics.setColour(128, 128, 128);
+		} else {
+			graphics.setColour(0, 128, 0);
+		}
+		graphics.print("Exit", window.height() + 8, window.height()/2 + 162);
+		graphics.setColour(0, 128, 0);
+		graphics.line(window.height(), window.height()/2 + 180, window.width() - 16, window.height()/2 + 180);
+		
+		
+		graphics.print(input.mouseX() + ", " + input.mouseY(), 0, 0);
 	}
 
 	@Override
