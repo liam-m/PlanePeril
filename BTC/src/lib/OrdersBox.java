4e54e2d72f4883b54b864831b8057d934eae85b7
Huw Taylor <huw_taylor@hotmail.co.uk>
2013-11-19 18:54:19 +0000

    fixed the ordersbox class to actually work

--- BTC/src/lib/OrdersBox.java
+++ BTC/src/lib/OrdersBox.java
@@ -5,11 +5,11 @@ import lib.jog.graphics;
 public class OrdersBox {
 	
 	private final int LINES;
+	private final char SEPARATOR = '|';
 	
 	private int _x, _y, _width, _height;
 	private String[] _orders;
 	private int _currentOrder;
-	private int _currentChar;
 	private double _timer;
 	private double _typeWait;
 	private boolean _typing;
@@ -23,29 +23,18 @@ public class OrdersBox {
 		_height = height;
 		_orders = new String[LINES];
 		_currentOrder = 0;
-		_currentChar = 0;
+		for (int i = 0; i < LINES; i ++) {
+			_orders[i] = "";
+		}
 		_timer = 0;
 		_typing = false;
-		_typeWait = 0.1;
+		_typeWait = 0.01;
 		_buffer = "";
 	}
 	
 	public void addOrder(String order) {
-		boolean gap = false;
-		for (int i = 0; i < LINES; i ++) {
-			if (_orders[i] == null && !gap) {
-				_orders[i] = order;
-				gap = true;
-			}
-		}
-		if (!gap) {
-			for (int i = 0; i < LINES-1; i++) {
-				_orders[i] = _orders[i+1];
-			}
-			_orders[LINES-1] = order;
-		}
+		_buffer += order + SEPARATOR;
 		_typing = true;
-		_buffer.concat("\n" + order);
 	}
 	
 	private int size() {
@@ -58,37 +47,40 @@ public class OrdersBox {
 	}
 	
 	public void update(double dt) {
-		if (_typing) {
-			_timer += dt;
-			if (_timer >= _typeWait) {
-				_timer -= _typeWait;
-				_currentChar += 1;
-				if (_currentChar == _orders[_currentOrder].length() - 1) {
-					_currentChar = 0;
-					_currentOrder += 1;
-					if (_currentOrder == size()) {
-						_typing = false;
-						if (_currentOrder == LINES) _currentOrder -= 1;
-					}
+		if (!_typing) return;
+		_timer += dt;
+		if (_timer >= _typeWait) {
+			_timer -= _typeWait;
+			if (_buffer.isEmpty()) {
+				_typing = false;
+			} else if (_buffer.charAt(0) == SEPARATOR) {
+				_currentOrder += 1;
+				_buffer = _buffer.substring(1);
+			} else {
+				if (_currentOrder >= LINES) {
+					ripple();
+					_currentOrder = LINES - 1;
 				}
+				_orders[_currentOrder] += _buffer.substring(0, 1);
+				_buffer = _buffer.substring(1);
 			}
 		}
+	}
+	
+	private void ripple() {
+		for (int i = 0; i < LINES-1; i ++) {
+			_orders[i] = _orders[i+1];
+		}
+		_orders[LINES-1] = "";
 	}
 	
 	public void draw() {
 		graphics.setColour(0, 128, 0);
 		graphics.rectangle(false, _x, _y, _width, _height);
-		for (int i = 0; i < _currentOrder; i ++) {
+		for (int i = 0; i < size(); i ++) {
 			if (_orders[i] != null) {
 				graphics.print(_orders[i], _x + 4, _y + 4 + (i * (_height-8) / LINES));
 			}
-		}
-		if (_typing) {
-			for (int i = 0; i < _currentChar; i ++) {
-				graphics.print(_orders[_currentOrder].substring(i, i+1), _x + 4 + (8 * i), _y + 4 + (_currentOrder * (_height-8) / LINES));
-			}
-		} else if (size() == LINES) {
-			graphics.print(_orders[_currentOrder], _x + 4, _y + 4 + ((_currentOrder) * (_height-8) / LINES));
 		}
 	}
 
