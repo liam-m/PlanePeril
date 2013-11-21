0b307c08962d29dca5dbed795c685e5f426f42a7
Huw Taylor <huw_taylor@hotmail.co.uk>
2013-11-21 20:24:31 +0000

    changed calculation to account for two planes telling it there was a separation violation

--- BTC/src/cls/Score.java
+++ BTC/src/cls/Score.java
@@ -2,32 +2,56 @@ package cls;
 
 public class Score {
 	
-	private double timePlayed;
-	private int flightsSuccessful;
+	private int _flightsSuccessful;
+	private double _timePlayed;
+	private double _timeManual;
+	private double _timeSeparationViolated;
+	private int _gameOvers;
 	
 	public Score(){
-		this.timePlayed = 0.00;
-		this.flightsSuccessful = 0;
+		_timePlayed = 0;
+		_timeManual = 0;
+		_timeSeparationViolated = 0;
+		_flightsSuccessful = 0;
+		_gameOvers = 0;
 	}
 
-	public double getTimePlayed() {
-		return timePlayed;
+	public double timePlayed() {
+		return _timePlayed;
 	}
 
-	public int getFlightsSuccessful() {
-		return flightsSuccessful;
+	public int flightsSuccessful() {
+		return _flightsSuccessful;
 	}
 
-	public void setTimePlayed(double timePlayed) {
-		this.timePlayed = timePlayed;
-	}
-
-	public void setFlightsSuccessful(int flightsSuccessful) {
-		this.flightsSuccessful = flightsSuccessful;
+	public void addTime(double dt) {
+		_timePlayed += dt;
 	}
 	
-	public void incFlightsSuccessful(){
-		this.flightsSuccessful ++;
+	public void addTimeManual(double dt) {
+		_timeManual += dt;
+	}
+	
+	public void addTimeViolated(double dt) {
+		_timeSeparationViolated += dt;
+	}
+
+	public void addFlight() {
+		_flightsSuccessful ++;
+	}
+	
+	public void addGameOver() {
+		_gameOvers ++;
+	}
+	
+	public int calculate() {
+		int score = 0;
+		score += (int)(_timePlayed);
+		score += (100 * _flightsSuccessful);
+		score -= (int)(_timeSeparationViolated);
+		score -= (int)(_timeManual);
+		score -= (1000 * _gameOvers);
+		return score;
 	}
 
 }
