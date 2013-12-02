8e4bdab1c77fa7400075216b441fde034ed61392
Huw Taylor <huw_taylor@hotmail.co.uk>
2013-12-02 21:23:58 +0000

    removed non-working sound effect stuff from jog.audio

--- BTC/src/lib/jog/audio.java
+++ BTC/src/lib/jog/audio.java
@@ -116,53 +116,6 @@ public abstract class audio {
 		
 	}
 	
-	
-	public static class SoundEffect {
-		
-		private org.newdawn.slick.Sound _source;
-		private boolean _looping;
-		private float _volume;
-		private float _pitch;
-		
-		private SoundEffect(String filepath, boolean looping) {
-			_looping = looping;
-			_volume = 1f;
-			_pitch = 1f;
-			try {
-				_source = new org.newdawn.slick.Sound(filepath);
-			} catch (SlickException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		public void play() {
-			if (_looping) {
-				_source.loop(_pitch, _volume);
-			} else {
-				_source.play(_pitch, _volume);
-			}
-		}
-
-		public void stop() {
-			_source.stop();
-		}
-
-		public void setVolume(float volume) {
-			_volume = volume;
-		}
-		
-		public float getVolume() {
-			return _volume;
-		}
-		
-	}
-	
-	public static SoundEffect newSoundEffect(String filepath, boolean loop) {
-		return new SoundEffect(filepath, loop);
-	}
-	public static SoundEffect newSoundEffect(String filepath) { return newSoundEffect(filepath, false); }
-	
-	
 	public static Music newMusic(String filepath, boolean stream, boolean loop) {
 		return new Music(filepath, stream, loop);
 	}
