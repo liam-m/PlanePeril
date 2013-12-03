b253f90bd95ec4914fd90e1828d7e5b5ead19e5f
Huw Taylor <huw_taylor@hotmail.co.uk>
2013-12-03 00:07:03 +0000

    refactored the variable names.

--- BTC/src/lib/Altimeter.java
+++ BTC/src/lib/Altimeter.java
@@ -6,89 +6,80 @@ import lib.jog.input.EventHandler;
 
 public class Altimeter implements EventHandler {
 	
-	private boolean _visible;
-	private cls.Aircraft _aircraft;
-	private double _x, _y, _width, _height;
-	private double _dragY;
+	private boolean isVisible;
+	private cls.Aircraft currentAircraft;
+	private double positionX, positionY, width, height;
 
-	public Altimeter(double x, double y, double width, double height) {
-		_x = x;
-		_y = y;
-		_width = width;
-		_height = height;
+	public Altimeter(double x, double y, double w, double h) {
+		positionX = x;
+		positionY = y;
+		width = w;
+		height = h;
 		hide();
 	}
 	
 	public void show(cls.Aircraft aircraft) {
 		if (aircraft == null) return;
-		_aircraft = aircraft;
-		_visible = true;
+		currentAircraft = aircraft;
+		isVisible = true;
 	}
 	
 	public void hide() {
-		_aircraft = null;
-		_visible = false;
+		currentAircraft = null;
+		isVisible = false;
 	}
 	
 	public boolean isMouseOver(int mx, int my) {
-		return (mx >= _x && mx <= _x + _width && my >= _y && my <= _y + _height);
+		return (mx >= positionX && mx <= positionX + width && my >= positionY && my <= positionY + height);
 	}
 	public boolean isMouseOver() { return isMouseOver(input.mouseX(), input.mouseY()); }
 
 	@Override
 	public void mousePressed(int key, int x, int y) {
-		if (!_visible) return;
-		if (key == input.MOUSE_LEFT && isMouseOver(x, y)) {
-			_dragY = y;
-		}
+		if (!isVisible) return;
 	}
 
 	@Override
 	public void mouseReleased(int key, int x, int y) {
-		if (!_visible) return;
-		if (key == input.MOUSE_LEFT && isMouseOver(x, y)) {
-			if (_dragY - y > 0) {
-				_aircraft.climb();
-			} else {
-				_aircraft.fall();
-			}
-		}
+		if (!isVisible) return;
 		if (key == input.MOUSE_WHEEL_DOWN) {
-			_aircraft.fall();
+			currentAircraft.fall();
 		}
 		if (key == input.MOUSE_WHEEL_UP) {
-			_aircraft.climb();
+			currentAircraft.climb();
 		}
 	}
 
 	@Override
-	public void keyPressed(int key) {
-		
-	}
+	public void keyPressed(int key) {}
 
 	@Override
-	public void keyReleased(int key) {
-		
-	}
+	public void keyReleased(int key) {}
 	
-	public void update(double dt) {
-		
-	}
+	public void update(double dt) {}
 	
 	public void draw() {
+		drawRectangle();
+		if (!isVisible) return;
+		drawPlaneIcon();
+	}
+	
+	private void drawRectangle() {
 		graphics.setColour(0, 128, 0);
-		graphics.rectangle(false, _x, _y, _width, _height);
-		if (!_visible) return;
+		graphics.rectangle(false, positionX, positionY, width, height);
+	}
+	
+	private void drawPlaneIcon() {
 		double r = 0;
-		if (_aircraft.isTurningLeft()) {
+		if (currentAircraft.isTurningLeft()) {
 			r = -Math.PI / 12;
-		} else if (_aircraft.isTurningRight()) {
+		} else if (currentAircraft.isTurningRight()) {
 			r = Math.PI / 12;
 		}
-		double x = _x + (_width / 2);
-		double y = _y + (_height / 2) - 16;
-		double wingLength = _width / 3;
-		double tailLength = _width / 9;
+		double x = positionX + (width / 2);
+		double y = positionY + (height / 2) - 16;
+		double wingLength = width / 3;
+		double tailLength = width / 9;
 		graphics.line(x, y, x + wingLength * Math.cos(r), y + wingLength * Math.sin(r));
 		r -= Math.PI / 2;
 		graphics.line(x, y, x + tailLength * Math.cos(r), y + tailLength * Math.sin(r));
@@ -98,7 +89,7 @@ public class Altimeter implements EventHandler {
 		graphics.circle(true, x, y, 4);
 		graphics.setColour(0, 128, 0);
 		graphics.circle(false, x, y, 4);
-		graphics.printCentred(String.valueOf(_aircraft.position().z()), _x, y+32, 1, _width);
+		graphics.printCentred(String.valueOf(currentAircraft.position().z()), positionX, y+32, 1, width);
 	}
 
 }
