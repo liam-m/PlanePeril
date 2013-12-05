3ac9105a358efad02326d451f97b1969cd0fa81e
Huw Taylor <huw_taylor@hotmail.co.uk>
2013-12-05 23:48:00 +0000

    added delay functionality

--- BTC/src/lib/TextBox.java
+++ BTC/src/lib/TextBox.java
@@ -10,21 +10,23 @@ import lib.jog.graphics;
  */
 public class TextBox {
 	
-	private final int LINES;
-	private final char SEPARATOR = '|';
-	private final double _typeWait = 0.01;
-	private final double _removalWait = 6;
+	protected final int LINES;
+	protected final char SEPARATOR = '|';
+	protected final char DELAY_START = '{';
+	protected final char DELAY_END = '}';
+	protected final double _typeWait = 0.01;
 	
-	private int _x, _y, _width, _height;
-	private String[] _orders;
-	private int _currentOrder;
-	private double _timer;
-	private double _removalTimer;
-	private boolean _typing;
-	private String _buffer;
+	protected int _x, _y, _width, _height;
+	protected String[] _orders;
+	protected int _currentOrder;
+	protected double _timer;
+	protected double _delayTimer;
+	protected boolean _isDelaying;
+	protected boolean _typing;
+	protected String _buffer;
 
 	/**
-	 * Constructor of a OrdersBox.
+	 * Constructor of a TextBox.
 	 * @param x the x coordinate to display the box.
 	 * @param y the y coordinate to display the box.
 	 * @param width the width the box wrap to.
@@ -43,16 +45,17 @@ public class TextBox {
 			_orders[i] = "";
 		}
 		_timer = 0;
-		_removalTimer = 0;
+		_delayTimer = 0;
+		_isDelaying = false;
 		_typing = false;
 		_buffer = "";
 	}
 	
 	/**
-	 * Adds an order to be displayed.
-	 * @param order the text to be written.
+	 * Adds a line of text to be displayed.
+	 * @param the text to be written.
 	 */
-	public void addOrder(String order) {
+	public void addText(String order) {
 		// Word Wrap
 		if (order.length()*8 > _width) {
 			String wrappedOrder = order.substring(0, (_width/8)-1);
@@ -60,15 +63,14 @@ public class TextBox {
 				wrappedOrder = wrappedOrder.substring(0, wrappedOrder.length()-1);
 			}
 			_buffer += wrappedOrder + SEPARATOR;
-			addOrder(order.substring(wrappedOrder.length()));
+			addText(order.substring(wrappedOrder.length()));
 		} else {
 			_buffer += order + SEPARATOR;
 		}
 		_typing = true;
-		_removalTimer = 0;
 	}
 	
-	private int linesBeingUsed() {
+	protected int linesBeingUsed() {
 		for (int i = 0; i < LINES; i ++) {
 			if (_orders[i] == null || _orders[i] == "") {
 				return i;
@@ -79,30 +81,40 @@ public class TextBox {
 	
 	/**
 	 * Accessed whether we have stopped typing and have no orders queued up.
-	 * @return whether the OrdersBox is up to date.
+	 * @return whether the TextBox is up to date.
 	 */
 	public boolean isUpToDate() {
 		return !_typing;
 	}
 	
 	/**
-	 * Updates the timer of the OrdersBox.
+	 * Updates the timer of the TextBox.
 	 * @param dt time since the last update call.
 	 */
 	public void update(double dt) {
-		if (!_typing) {
-			_removalTimer += dt;
-			if (_removalTimer >= _removalWait) {
-				_removalTimer -= _removalWait;
-				ripple();
+		if (_isDelaying) {
+			if (_delayTimer <= 0) {
+				_isDelaying = false;
+			} else {
+				_delayTimer = Math.max(0, _delayTimer - dt);
+				return;
 			}
-			return;
 		}
 		_timer += dt;
 		if (_timer >= _typeWait) {
 			_timer -= _typeWait;
 			if (_buffer.isEmpty()) {
 				_typing = false;
+			} else if (_buffer.charAt(0) == DELAY_START) {
+				_buffer = _buffer.substring(1);
+				_isDelaying = true;
+				String delay = "";
+				while (_buffer.charAt(0) != DELAY_END) {
+					delay += _buffer.charAt(0);
+					_buffer = _buffer.substring(1);
+				}
+				_buffer = _buffer.substring(1);
+				_delayTimer = Double.parseDouble(delay);
 			} else if (_buffer.charAt(0) == SEPARATOR) {
 				_currentOrder += 1;
 				_buffer = _buffer.substring(1);
@@ -116,7 +128,11 @@ public class TextBox {
 		}
 	}
 	
-	private void ripple() {
+	public void delay(double duration) {
+		_buffer += DELAY_START + String.valueOf(duration) + DELAY_END;
+	}
+	
+	protected void ripple() {
 		for (int i = 0; i < LINES-1; i ++) {
 			_orders[i] = _orders[i+1];
 		}
@@ -125,7 +141,7 @@ public class TextBox {
 	}
 	
 	/**
-	 * Prints the currently available characters of the OrdersBox.
+	 * Prints the currently available characters of the TextBox.
 	 */
 	public void draw() {
 		graphics.setColour(0, 128, 0);
