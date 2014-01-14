45c99f2d1b49b69a2b88c2c1188f57554f5f4ec1
Huw Taylor <huw_taylor@hotmail.co.uk>
2014-01-14 22:33:49 +0000

    made credits scrolling text, not textboxes. and it now loops

--- BTC/src/scn/Credits.java
+++ BTC/src/scn/Credits.java
@@ -1,6 +1,8 @@
 package scn;
 
-import lib.TextBox;
+import java.io.File;
+
+import lib.jog.audio;
 import lib.jog.graphics;
 import lib.jog.input;
 import lib.jog.window;
@@ -8,8 +10,11 @@ import btc.Main;
 
 public class Credits extends Scene {
 	
+	private final static int SCROLL_SPEED = 64;
+	
 	private float speed;
-	private lib.TextBox[] people;
+	private double scrollPosition;
+	private audio.Music music;
 
 	public Credits(Main main) {
 		super(main);
@@ -34,59 +39,82 @@ public class Credits extends Scene {
 	@Override
 	public void start() {
 		speed = 1f;
-		people = new TextBox[6];
-		// dimensions of each textbox
-		int w = window.width() / 6;
-		int h = 96;
-		// columns of textboxs
-		int columns = 2;
-		int rows = people.length / columns;
-		// calculate positions;
-		int paddingW = (window.width() - (w * columns)) / (columns + 1);
-		int paddingH = (window.height() - (h * rows)) / (rows + 1);
-		System.out.println("" + paddingW + ", " + paddingH);
-		for (int i = 0; i < people.length; i ++) {
-			int x = paddingW + (i % columns) * (w + paddingW);
-			int y = paddingH + (i / columns) * (h + paddingH);
-			System.out.println("" + x + ", " + y + ", " + w + ", " + h);
-			people[i] = new TextBox(x, y, w, h, 4);
-			people[i].setSpeed(0.03);
-		}
-		// Josh Adams
-		people[0].addText("Josh Adams");
-		// Gareth Handley
-		people[1].addText("Gareth Handley");
-		// Sanjit Samaddar
-		people[2].addText("Sanjit Samaddar");
-		// Alex Stuart
-		people[3].addText("Alex Stuart");
-		// Huw Taylor
-		people[4].addText("Huw Taylor");
-		// Stephen Webb
-		people[5].addText("Stephen Webb");
+		scrollPosition = -window.height();
+		music = audio.newMusic("sfx" + File.separator + "piano.ogg");
+		music.play();
 	}
 
 	@Override
 	public void update(double dt) {
 		boolean hurried = input.isKeyDown(input.KEY_SPACE) || input.isMouseDown(input.MOUSE_LEFT);
 		speed = hurried ? 4f : 1f;
-		for (int i = 0; i < people.length; i ++) {
-			if (i == 0 || people[i-1].isUpToDate()) {
-				people[i].update(dt * speed);
-			}
-		}
+		scrollPosition += SCROLL_SPEED * dt * speed;
+		if (scrollPosition > 1100) scrollPosition = -window.height();
 	}
 
 	@Override
 	public void draw() {
+		int gap = 64;
+		int currentHeight = 0;
 		graphics.setColour(0, 128, 0);
-		graphics.printCentred("Bear Traffic Controller", 0, 48, 2, window.width());
-		for (TextBox person : people) {
-			person.draw();
-		}
+		graphics.push();
+		graphics.translate(0, scrollPosition);
+		currentHeight += gap;
+		graphics.printCentred("Bear Traffic Controller", 0, currentHeight, 3, window.width());
+		currentHeight += gap * 2;
+		
+		graphics.printCentred("Programming", 0, currentHeight, 2, window.width());
+		graphics.printCentred("___________", 0, currentHeight + 8, 2, window.width());
+		graphics.printCentred("__________", 4, currentHeight + 8, 2, window.width());
+		currentHeight += gap;
+		graphics.printCentred("Josh Adams", 0, currentHeight, 2, window.width()/3);
+		graphics.printCentred("Huw Taylor", window.width()/3, currentHeight, 2, window.width()/3);
+		graphics.printCentred("Stephen Webb", 2*window.width()/3, currentHeight, 2, window.width()/3);
+		currentHeight += gap * 2;
+		
+		graphics.printCentred("Website", 0, currentHeight, 2, window.width());
+		graphics.printCentred("_______", 0, currentHeight + 8, 2, window.width());
+		graphics.printCentred("______", 4, currentHeight + 8, 2, window.width());
+		currentHeight += gap;
+		graphics.printCentred("Gareth Handley", 0, currentHeight, 2, window.width());
+		currentHeight += gap * 2;
+		
+		graphics.printCentred("Testing", 0, currentHeight, 2, window.width());
+		graphics.printCentred("_______", 0, currentHeight + 8, 2, window.width());
+		graphics.printCentred("______", 4, currentHeight + 8, 2, window.width());
+		currentHeight += gap;
+		graphics.printCentred("Sanjit Samaddar", 0, currentHeight, 2, window.width()/2);
+		graphics.printCentred("Alex Stuart", window.width()/2, currentHeight, 2, window.width()/2);
+		currentHeight += gap * 2;
+
+		graphics.printCentred("Music", 0, currentHeight, 2, window.width());
+		graphics.printCentred("_____", 0, currentHeight + 8, 2, window.width());
+		graphics.printCentred("____", 4, currentHeight + 8, 2, window.width());
+		currentHeight += gap;
+		graphics.printCentred("Gypsy Shoegazer", 0, currentHeight, 2, window.width()/2);
+		graphics.printCentred("Beep SFX", window.width()/2, currentHeight, 2, window.width()/2);
+		currentHeight += gap / 2;
+		graphics.printCentred("Kevin MacLeod", 0, currentHeight, 2, window.width()/2);
+		graphics.printCentred("Partners in Rhyme", window.width()/2, currentHeight, 2, window.width()/2);
+		currentHeight += gap * 2;
+		
+		graphics.printCentred("External Libraries", 0, currentHeight, 2, window.width());
+		graphics.printCentred("__________________", 0, currentHeight + 8, 2, window.width());
+		graphics.printCentred("_________________", 4, currentHeight + 8, 2, window.width());
+		currentHeight += gap;
+		graphics.printCentred("LWJGL", 0, currentHeight, 2, window.width()/3);
+		graphics.printCentred("Slick2D", window.width()/3, currentHeight, 2, window.width()/3);
+		graphics.printCentred("JOG", 2*window.width()/3, currentHeight, 2, window.width()/3);
+		currentHeight += gap * 2;
+		
+		graphics.pop();
+		
+		graphics.print("" + scrollPosition, 0, 0);
 	}
 
 	@Override
-	public void close() {}
+	public void close() {
+		music.stop();
+	}
 
 }
