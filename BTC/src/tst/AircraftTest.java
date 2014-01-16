6958131706a3b9db3d3f6c5780fdd214c79b9b39
Alex Stewart <as1481@york.ac.uk>
2014-01-16 17:30:37 +0000

    Removed MainTest class
    Nothing in this class I feel I can test

--- BTC/src/tst/AircraftTest.java
+++ BTC/src/tst/AircraftTest.java
@@ -1,6 +1,9 @@
 package tst;
 
 import static org.junit.Assert.*;
+
+import java.io.File;
+
 import lib.jog.graphics;
 
 import org.junit.Test;
@@ -16,7 +19,7 @@ public class AircraftTest {
 	@Test
 	public void testGePosition() {
 		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
-		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), graphics.newImage(null), 10.0, waypointList, 1);
+		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), graphics.newImage("gfx" + File.separator + "plane.png"), 10.0, waypointList, 1);
 		Vector resultPosition = testAircraft.position();
 		assertTrue("x = 0, y = 0, z = 0", (0 == resultPosition.x()) && (0 == resultPosition.y()) && (0 == resultPosition.z()));
 	}
@@ -24,14 +27,14 @@ public class AircraftTest {
 	@Test
 	public void testGetName() {
 		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
-		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), graphics.newImage(null), 10.0, waypointList, 1);
+		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), graphics.newImage("gfx" + File.separator + "plane.png"), 10.0, waypointList, 1);
 		assertTrue("Name = testAircraft", "testAircraft" == testAircraft.name());
 	}
 	// Test getOriginName function
 	@Test
 	public void testGetOriginName(){
 		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
-		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), graphics.newImage(null), 10.0, waypointList, 1);
+		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), graphics.newImage("gfx" + File.separator + "plane.png"), 10.0, waypointList, 1);
 		assertTrue("Origin name = Berlin", "Berlin" == testAircraft.originName());
 	}
 
