a8d9ab08153540b97b61bee2227c8d45fa8a7420
Alex Stewart <as1481@york.ac.uk>
2014-01-19 22:15:12 +0000

    Added multiple aircraft class tests
    Added multiple tests for the aircraft class

--- BTC/src/tst/AircraftTest.java
+++ BTC/src/tst/AircraftTest.java
@@ -9,62 +9,90 @@ import cls.Waypoint;
 import cls.Vector;
 
 public class AircraftTest {
+	
+	// Create test aircraft
+	private Aircraft generateTestAircraft()
+	{
+		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
+		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), null, 10.0, waypointList, 1);
+		return testAircraft;
+	}
 
-	// Test Get functions
+	// Test get functions
 	// Test getPosition function
 	@Test
 	public void testGePosition() {
-		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
-		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), null, 10.0, waypointList, 1);
+		Aircraft testAircraft = generateTestAircraft();
 		Vector resultPosition = testAircraft.position();
-		assertTrue("x >= -64 and xy <= 64, y = 0, z = 28,000 or z = 30,000", ((0 == resultPosition.y()) && (64 >= resultPosition.x()) && (-64 <= resultPosition.x()) && ((28000 == resultPosition.z()) || (30000 == resultPosition.z()))));
+		assertTrue("x >= -128 and xy <= 27, y = 0, z = 28,000 or z = 30,000", ((0 == resultPosition.y()) && (128 >= resultPosition.x()) && (-128 <= resultPosition.x()) && ((28000 == resultPosition.z()) || (30000 == resultPosition.z()))));
 	}
 	// Test getName function
 	@Test
 	public void testGetName() {
-		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
-		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), null, 10.0, waypointList, 1);
+		Aircraft testAircraft = generateTestAircraft();
 		String name = testAircraft.name();
 		assertTrue("Name = testAircraft", "testAircraft" == name);
 	}
 	// Test getOriginName function
 	@Test
 	public void testGetOriginName(){
-		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
-		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), null, 10.0, waypointList, 1);
+		Aircraft testAircraft = generateTestAircraft();
 		String name = testAircraft.originName();
 		assertTrue("Origin name = Dublin", "Dublin" == name);
 	}
 	// Test getDestinationName function
 	@Test
 	public void testGetDestinationName(){
-		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
-		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), null, 10.0, waypointList, 1);
+		Aircraft testAircraft = generateTestAircraft();
 		String name = testAircraft.destinationName();
 		assertTrue("Destination name = Berlin", "Berlin" == name);
 	}
 	// Test getIsFinished function
 	@Test
 	public void testGetIsFinishedName(){
-		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
-		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), null, 10.0, waypointList, 1);
+		Aircraft testAircraft = generateTestAircraft();
 		boolean status = testAircraft.isFinished();
 		assertTrue("Finished = false", false == status);
 	}
 	// Test getIsManuallyControlled function
 	@Test
 	public void testIsManuallyControlled(){
-		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
-		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), null, 10.0, waypointList, 1);
+		Aircraft testAircraft = generateTestAircraft();
 		boolean status = testAircraft.isManuallyControlled();
 		assertTrue("Manually controlled = false", false == status);
 	}
 	// Test getSpeed function
 	@Test
 	public void testGetSpeed(){
-		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
-		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(100,100, true), new Waypoint(0,0, true), null, 10.0, waypointList, 1);
-		double speed = testAircraft.speed();
+		Aircraft testAircraft = generateTestAircraft();
+		double speed = (int) (testAircraft.speed() + 0.5);
 		assertTrue("Speed = 20", speed == 20.0);
 	}
+	// Test getAltitudeState
+	@Test
+	public void testAltitudeState(){
+		Aircraft testAircraft = generateTestAircraft();
+		testAircraft.setAltitudeState(1);
+		int altState = testAircraft.altitudeState();
+		assertTrue("Altitude State = 1", altState == 1);
+	}
+	// Test outOfBounds
+	@Test
+	public void testOutOfBounds(){
+		Waypoint[] waypointList = new Waypoint[]{new Waypoint(0, 0, true), new Waypoint(100, 100, true), new Waypoint(25, 75, false), new Waypoint(75, 25, false), new Waypoint(50,50, false)};
+		Aircraft testAircraft = new Aircraft("testAircraft", "Berlin", "Dublin", new Waypoint(-10000,-10000, true), new Waypoint(0,0, true), null, 10.0, waypointList, 1);
+		boolean x = testAircraft.outOfBounds();
+		assertTrue("Out of bounds = false", x == true);
+	}
+	
+	// Test set methods
+	// Test setAltitudeState
+	@Test
+	public void testSetAltitudeState(){
+		Aircraft testAircraft = generateTestAircraft();
+		testAircraft.setAltitudeState(1);
+		int altState = testAircraft.altitudeState();
+		assertTrue("Altitude State = 1", altState == 1);
+	}
+
 }
