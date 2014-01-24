afa253603e5514ddd751ba1aabdff51625fec1a1
IMP1 <huw_taylor@hotmail.co.uk>
2014-01-24 11:19:46 +0000

    updates SpriteAnimation to not be hard coded to one image

--- BTC/src/lib/SpriteAnimation.java
+++ BTC/src/lib/SpriteAnimation.java
@@ -7,7 +7,7 @@ import lib.jog.graphics.Quad;
 public class SpriteAnimation {
 
 	private Image image;		 // the animation sequence
-	private Quad quad;	         // the rectangle to be drawn from the animation bitmap
+	private Quad[] quads;	         // the rectangle to be drawn from the animation bitmap
 	private int frameCount;		 // number of frames in animation
 	private int currentFrame;	 // the current frame
 	private double framePeriod;	 // milliseconds between each frame (1000/fps)
@@ -21,31 +21,64 @@ public class SpriteAnimation {
 	private int y;				 // the Y coordinate of the object (top left of the image)
 	private double gameTime;	 // tracks how long a frame has been shown for. Updated by the parent scene update(dt)
 	private double imageW, imageH;
+	private boolean isLooping;
 	
-	public SpriteAnimation(Image image, int x, int y, int fps, int frameCount){
+	/**
+	 * <h1>Sprite Animation</h1>
+	 * <p>Creates an animation class</p>
+	 * @author Stephen Webb
+	 * @param image image from which the quads are taken.
+	 * @param x the x position to draw the animation.
+	 * @param y the y position to draw the animation.
+	 * @param fps how many animation frames to draw per second.
+	 * @param frameCount how many frames the animation comprises.
+	 * @param framesWide how many frames wide the image is
+	 * @param framesHigh how many frames high the image is
+	 * @param looping whether to loop the animation
+	 */
+	public SpriteAnimation(Image image, int x, int y, int fps, int frameCount, int framesWide, int framesHigh, boolean looping){
 		this.image = image;
-		imageW = 2520;
-		imageH = 200;
+		imageW = image.width();
+		imageH = image.height();
 		this.x = x;
 		this.y = y;
 		this.frameCount = frameCount;
 		currentFrame = 0;
-		spriteWidth = 140;
-		spriteHeight = 200;
+		spriteWidth = imageW / framesWide;
+		spriteHeight = imageH / framesHigh;
 		framePeriod = 1.0/fps;
 		gameTime = 0;
-		quad = getQuad(0);
+		quads = new Quad[frameCount];
+		for (int n = 0; n < frameCount; n ++) {
+			int i = n % framesWide;
+			int j = n / framesWide;
+			quads[n] = graphics.newQuad(i * spriteWidth, j * spriteWidth, spriteWidth, spriteHeight, imageW, imageH);
+		}
+		isLooping = looping;
 		hasFinished = false;
 	}
+	
 	/**
-	 * Gets a new Quad of the image to be drawn
-	 * @param currentFrame the currentFrame of the image to draw
-	 * @return the new quad
+	 * <h1>Sprite Animation</h1>
+	 * <p>Creates an animation class</p>
+	 * @author Stephen Webb
+	 * @param imageFilepath the filepath at which the image is.  
+	 * @param x the x position to draw the animation.
+	 * @param y the y position to draw the animation.
+	 * @param fps how many animation frames to draw per second.
+	 * @param frameCount how many frames the animation comprises.
+	 * @param framesWide how many frames wide the image is
+	 * @param framesHigh how many frames high the image is
+	 * @param looping whether to loop the animation
 	 */
-	public Quad getQuad(int currentFrame){
-		return graphics.newQuad(currentFrame * spriteWidth, 0, spriteWidth, spriteHeight, imageW * 1.62, imageH);
+	public SpriteAnimation(String imageFilepath, int x, int y, int fps, int frameCount, int framesWide, int framesHigh, boolean looping){
+		new SpriteAnimation(graphics.newImage(imageFilepath), x, y, fps, frameCount, framesWide, framesHigh, looping);
 	}
 	
+	/**
+	 * updates the timer and changes the frame if necessary
+	 * @param dt time in seconds since last update
+	 */
 	public void update(double dt) {
 		if (hasFinished) return;
 		
@@ -53,18 +86,28 @@ public class SpriteAnimation {
 		if (gameTime > framePeriod) { // frame period exceeded
 			gameTime = 0; //reset timer
 			currentFrame++; //increment frame
-			if (currentFrame > frameCount) {
-				hasFinished = true;
+			if (currentFrame >= frameCount) {
+				if (!isLooping) {
+					hasFinished = true;
+				} else {
+					currentFrame = 0;
+				}
 			}
-			quad = getQuad(currentFrame); //get new quad for new frame
 		}
 	}
 	
+	/**
+	 * draws the animation
+	 */
 	public void draw() {
 		if (hasFinished) return;
-		graphics.drawq(image, quad, x, y);
+		graphics.drawq(image, quads[currentFrame], x, y);
 	}
 	
+	/**
+	 * 
+	 * @return whether the animation has finished
+	 */
 	public boolean hasFinished() {
 		return hasFinished;
 	}
