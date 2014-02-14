6581afc7981c930e1351356419d8c805de5b5c71
Leslie Hor <Leslie.Hor@Gmail.com>
2014-02-14 15:18:03 +0000

    Testing: Started aircraft tests, added an aircraft to the airport tests.

--- BTC/src/tst/AircraftTest2.java
+++ BTC/src/tst/AircraftTest2.java
@@ -1,6 +1,11 @@
 package tst;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,8 +13,63 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import cls.Aircraft;
+import cls.Airport;
+import cls.HoldingWaypoint;
+import cls.Vector;
+import cls.Waypoint;
+import cls.Waypoint.WaypointType;
 
 public class AircraftTest2 {
+
+	public Airport airport;
+
+	public ArrayList<Aircraft> aircraftInAirspace;
+
+	public final Waypoint[] locationWaypoints = new Waypoint[] {
+	/* A set of Waypoints which are origin / destination points */
+
+			// top left
+			new Waypoint(10, 10, WaypointType.ENTRY_EXIT, "Test Entry 1"),
+
+			// bottom left
+			new Waypoint(10, 1000, WaypointType.ENTRY_EXIT, "Test Entry 2"),
+
+			// top right
+			new Waypoint(1000, 10, WaypointType.ENTRY_EXIT, "Test Entry 3"),
+
+			// bottom right
+			new Waypoint(1000, 1000, WaypointType.ENTRY_EXIT, "Test Entry 4"),
+
+			// The airport
+			airport = new Airport(500, 500, "Airport"), };
+
+	public Waypoint[] airspaceWaypoints = new Waypoint[] {
+
+			// airspace waypoints
+			new Waypoint(160, 174), // 0
+			new Waypoint(383, 481), // 1
+			new Waypoint(502, 274), // 2
+			new Waypoint(632, 125), // 3
+			new Waypoint(949, 96), // 4
+			new Waypoint(698, 430), // 4.1
+			new Waypoint(909, 481), // 5
+
+			// destination/origin waypoints - present in this list for
+			// pathfinding.
+			locationWaypoints[0], // 10
+			locationWaypoints[1], // 11
+			locationWaypoints[2], // 12
+			locationWaypoints[3], // 13
+			locationWaypoints[4], // 14 - Airport
+	};
+
+	public Aircraft testAircraft;
+
+	public ArrayList<HoldingWaypoint> holdingWaypoints = new ArrayList<HoldingWaypoint>();
+
+	public final Waypoint takeoffWaypoint = new Waypoint(
+			airport.position().x() - 60, airport.position().y());
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -21,6 +81,31 @@ public class AircraftTest2 {
 
 	@Before
 	public void setUp() throws Exception {
+		holdingWaypoints.add(new HoldingWaypoint(locationWaypoints[4]
+				.position().x() - 100,
+				locationWaypoints[4].position().y() - 100));
+		holdingWaypoints.add(new HoldingWaypoint(locationWaypoints[4]
+				.position().x() + 100,
+				locationWaypoints[4].position().y() - 100));
+		holdingWaypoints.add(new HoldingWaypoint(locationWaypoints[4]
+				.position().x() + 100,
+				locationWaypoints[4].position().y() + 100));
+		holdingWaypoints.add(new HoldingWaypoint(locationWaypoints[4]
+				.position().x() - 100,
+				locationWaypoints[4].position().y() + 100));
+
+		// Initialise values of setNextWaypoint.
+		holdingWaypoints.get(0).setNextWaypoint(holdingWaypoints.get(1));
+		holdingWaypoints.get(1).setNextWaypoint(holdingWaypoints.get(2));
+		holdingWaypoints.get(2).setNextWaypoint(holdingWaypoints.get(3));
+		holdingWaypoints.get(3).setNextWaypoint(holdingWaypoints.get(0));
+
+		aircraftInAirspace = new ArrayList<Aircraft>();
+
+		testAircraft = new Aircraft("testAircraft", "Exit", "Entry",
+				locationWaypoints[0], locationWaypoints[1], null, 10.0,
+				airspaceWaypoints, 1, holdingWaypoints, takeoffWaypoint,
+				aircraftInAirspace);
 	}
 
 	@After
@@ -34,72 +119,104 @@ public class AircraftTest2 {
 
 	@Test
 	public void testPosition() {
-		fail("Not yet implemented");
+		assertEquals(testAircraft.position().x(), 10, 0);
+		assertEquals(testAircraft.position().y(), 1000, 0);
 	}
 
 	@Test
 	public void testName() {
-		fail("Not yet implemented");
+		assertTrue(testAircraft.name().equals("testAircraft"));
 	}
 
 	@Test
 	public void testOriginName() {
-		fail("Not yet implemented");
+		assertTrue(testAircraft.originName().equals("Entry"));
 	}
 
 	@Test
 	public void testDestinationName() {
-		fail("Not yet implemented");
+		assertTrue(testAircraft.destinationName().equals("Exit"));
 	}
 
 	@Test
 	public void testIsFinished() {
-		fail("Not yet implemented");
+		assertFalse(testAircraft.isFinished());
+
+		// need another test. collide it with another aircraft, and test if this
+		// returns true.
 	}
 
 	@Test
 	public void testAtAirport() {
-		fail("Not yet implemented");
+		assertFalse(testAircraft.atAirport());
+
+		// need another test. put aircraft into airport, test if this returns
+		// true.
 	}
 
 	@Test
 	public void testIsManuallyControlled() {
-		fail("Not yet implemented");
+		assertFalse(testAircraft.isManuallyControlled());
+
+		// need another test
 	}
 
 	@Test
 	public void testIsLanding() {
-		fail("Not yet implemented");
+		assertFalse(testAircraft.isLanding());
+
+		// need another test
 	}
 
 	@Test
 	public void testOutOfBounds() {
-		fail("Not yet implemented");
+		assertFalse(testAircraft.outOfBounds());
+		// need another test
 	}
 
 	@Test
 	public void testBearing() {
+		// TEST NOT WORKING
+
+		// System.out.println(testAircraft.bearing());
+		//
+		// Waypoint currentTarget = testAircraft.
+		//
+		// double x = currentTarget.position().x() - position.x();
+		// double y = currentTarget.position().y() - position.y();
+		//
+		// velocity = new Vector(x, y, 0).normalise().scaleBy(speed);
+
 		fail("Not yet implemented");
 	}
 
 	@Test
 	public void testSpeed() {
-		fail("Not yet implemented");
+		// TEST NOT WORKING
+
+		// System.out.println(testAircraft.speed());
+		assertEquals(testAircraft.speed(), 10.0, 0);
 	}
 
 	@Test
 	public void testIsAt() {
-		fail("Not yet implemented");
+		Vector point1 = new Vector(10.0, 10.0, 10.0);
+
+		assertFalse(testAircraft.isAt(point1));
+
+		Vector point2 = new Vector(10.0, 1000.0, 10.0);
+
+		assertTrue(testAircraft.isAt(point2));
 	}
 
 	@Test
 	public void testIsTurningLeft() {
-		fail("Not yet implemented");
+		assertFalse(testAircraft.isTurningLeft());
 	}
 
 	@Test
 	public void testIsTurningRight() {
-		fail("Not yet implemented");
+		assertFalse(testAircraft.isTurningRight());
 	}
 
 	@Test
--- BTC/src/tst/AirportTest.java
+++ BTC/src/tst/AirportTest.java
@@ -1,6 +1,8 @@
 package tst;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -8,14 +10,66 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import cls.Aircraft;
 import cls.Airport;
+import cls.HoldingWaypoint;
+import cls.Waypoint;
+import cls.Waypoint.WaypointType;
 
 public class AirportTest {
 
+	public Airport airport;
+
+	public ArrayList<Aircraft> aircraftInAirspace;
+
+	public final Waypoint[] locationWaypoints = new Waypoint[] {
+	/* A set of Waypoints which are origin / destination points */
+
+			// top left
+			new Waypoint(10, 10, WaypointType.ENTRY_EXIT, "Test Entry 1"),
+
+			// bottom left
+			new Waypoint(10, 1000, WaypointType.ENTRY_EXIT, "Test Entry 2"),
+
+			// top right
+			new Waypoint(1000, 10, WaypointType.ENTRY_EXIT, "Test Entry 3"),
+
+			// bottom right
+			new Waypoint(1000, 1000, WaypointType.ENTRY_EXIT, "Test Entry 4"),
+
+			// The airport
+			airport = new Airport(500, 500, "Airport"), };
+
+	public Waypoint[] airspaceWaypoints = new Waypoint[] {
+
+			// airspace waypoints
+			new Waypoint(160, 174), // 0
+			new Waypoint(383, 481), // 1
+			new Waypoint(502, 274), // 2
+			new Waypoint(632, 125), // 3
+			new Waypoint(949, 96), // 4
+			new Waypoint(698, 430), // 4.1
+			new Waypoint(909, 481), // 5
+
+			// destination/origin waypoints - present in this list for
+			// pathfinding.
+			locationWaypoints[0], // 10
+			locationWaypoints[1], // 11
+			locationWaypoints[2], // 12
+			locationWaypoints[3], // 13
+			locationWaypoints[4], // 14 - Airport
+	};
+
+	public Aircraft testAircraft;
+
+	public ArrayList<HoldingWaypoint> holdingWaypoints = new ArrayList<HoldingWaypoint>();
+
+	public final Waypoint takeoffWaypoint = new Waypoint(
+			airport.position().x() - 60, airport.position().y());
+
 	public AirportTest() {
 		Airport testAirport = new Airport(10, 10, "INI");
-		
-		
+
 	}
 
 	@BeforeClass
@@ -28,6 +82,31 @@ public class AirportTest {
 
 	@Before
 	public void setUp() throws Exception {
+		holdingWaypoints.add(new HoldingWaypoint(locationWaypoints[4]
+				.position().x() - 100,
+				locationWaypoints[4].position().y() - 100));
+		holdingWaypoints.add(new HoldingWaypoint(locationWaypoints[4]
+				.position().x() + 100,
+				locationWaypoints[4].position().y() - 100));
+		holdingWaypoints.add(new HoldingWaypoint(locationWaypoints[4]
+				.position().x() + 100,
+				locationWaypoints[4].position().y() + 100));
+		holdingWaypoints.add(new HoldingWaypoint(locationWaypoints[4]
+				.position().x() - 100,
+				locationWaypoints[4].position().y() + 100));
+
+		// Initialise values of setNextWaypoint.
+		holdingWaypoints.get(0).setNextWaypoint(holdingWaypoints.get(1));
+		holdingWaypoints.get(1).setNextWaypoint(holdingWaypoints.get(2));
+		holdingWaypoints.get(2).setNextWaypoint(holdingWaypoints.get(3));
+		holdingWaypoints.get(3).setNextWaypoint(holdingWaypoints.get(0));
+
+		aircraftInAirspace = new ArrayList<Aircraft>();
+
+		testAircraft = new Aircraft("testAircraft", "Exit", "Entry",
+				locationWaypoints[0], locationWaypoints[1], null, 10.0,
+				airspaceWaypoints, 1, holdingWaypoints, takeoffWaypoint,
+				aircraftInAirspace);
 	}
 
 	@After
@@ -36,13 +115,13 @@ public class AirportTest {
 
 	@Test
 	public void testAirport() {
-		//If initialisation works, this works.
-		//fail("Not yet implemented");
+		// If initialisation works, this works.
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testInsertAircraft() {
-		
+
 		fail("Not yet implemented");
 	}
 
