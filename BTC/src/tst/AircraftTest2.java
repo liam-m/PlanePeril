d6563c06714cf2698324ddd685f65c97b2332d63
josephhamm <Joseph@10.240.39.103>
2014-02-14 13:58:42 +0000

    JUnit fix?

---
+++ BTC/src/tst/AircraftTest2.java
@@ -0,0 +1,225 @@
+package tst;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class AircraftTest2 {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAircraft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPosition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOriginName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDestinationName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsFinished() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAtAirport() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsManuallyControlled() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsLanding() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOutOfBounds() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBearing() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSpeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsTurningLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsTurningRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFlightPathContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAlterPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsMouseOverIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsMouseOver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTurnLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTurnRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDraw() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDrawCompass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDrawFlightPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDrawModifiedPath() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindGreedyRoute() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateCollisions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToggleManualControl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetManualControl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToggleLand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBearing() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClimb() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFall() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDecreaseTargetAltitude() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIncreaseTargetAltitude() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAltitude() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPoints() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDestination() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ BTC/src/tst/AirportTest.java
@@ -0,0 +1,137 @@
+/**
+ * 
+ */
+package tst;
+
+import cls.Aircraft;
+import cls.Airport;
+import cls.Waypoint;
+import cls.Waypoint.WaypointType;
+import junit.framework.TestCase;
+
+/**
+ * @author Joseph
+ *
+ */
+public class AirportTest extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public AirportTest(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link cls.Airport#Airport(double, double, java.lang.String)}.
+	 */
+	public void testAirport() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Airport#insertAircraft(cls.Aircraft)}.
+	 */
+	public void testInsertAircraft() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Airport#takeoff()}.
+	 */
+	public void testTakeoff() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Airport#drawAirportInfo()}.
+	 */
+	public void testDrawAirportInfo() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Waypoint#Waypoint(double, double, cls.Waypoint.WaypointType, java.lang.String)}.
+	 */
+	public void testWaypointDoubleDoubleWaypointTypeString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Waypoint#Waypoint(double, double)}.
+	 */
+	public void testWaypointDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Waypoint#position()}.
+	 */
+	public void testPosition() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Waypoint#isMouseOver(int, int)}.
+	 */
+	public void testIsMouseOver() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Waypoint#getType()}.
+	 */
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Waypoint#getCost(cls.Waypoint)}.
+	 */
+	public void testGetCost() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Waypoint#getCostBetween(cls.Waypoint, cls.Waypoint)}.
+	 */
+	public void testGetCostBetween() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Waypoint#getName()}.
+	 */
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Waypoint#draw(double, double)}.
+	 */
+	public void testDrawDoubleDouble() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link cls.Waypoint#draw()}.
+	 */
+	public void testDraw() {
+		fail("Not yet implemented");
+	}
+
+}
