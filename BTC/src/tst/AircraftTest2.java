b72b6b1cd588071bfe015673359f8f2dddb6c076
Leslie Hor <Leslie.Hor@Gmail.com>
2014-02-15 17:48:26 +0000

    Finishing aircrafts test.

--- BTC/src/tst/AircraftTest2.java
+++ BTC/src/tst/AircraftTest2.java
@@ -3,7 +3,6 @@ package tst;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 
@@ -114,7 +113,7 @@ public class AircraftTest2 {
 
 	@Test
 	public void testAircraft() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
@@ -190,9 +189,6 @@ public class AircraftTest2 {
 
 		for (int x = 0; x < 20; x = x + 1) {
 			testAircraft.update(0.3);
-
-			System.out.println(testAircraft.outOfBounds());
-			System.out.println(testAircraft.position().x());
 		}
 
 		assertTrue(testAircraft.outOfBounds());
@@ -200,36 +196,13 @@ public class AircraftTest2 {
 
 	@Test
 	public void testBearing() {
-<<<<<<< HEAD
 		Vector point2 = new Vector(20.0, 20.0, 20.0);
 		testAircraft.isAt(point2);
 		assertEquals(testAircraft.bearing(), -0.9476366, 0.1);
-		//fail("Not yet implemented");
-=======
-		// TEST NOT WORKING
-
-		// Waypoint currentTarget = airspaceWaypoints[1];
-		//
-		// double x = currentTarget.position().x() -
-		// testAircraft.position().x();
-		// double y = currentTarget.position().y() -
-		// testAircraft.position().y();
-		//
-		// Vector velocity = new Vector(x, y, 0).normalise().scaleBy(10.0);
-		// velocity = velocity.scaleBy(2);
-		//
-		// System.out.println(Math.atan2(velocity.x(), velocity.y()));
-		// System.out.println(testAircraft.speed());
-		// System.out.println(testAircraft.bearing());
-
-		fail("Not yet implemented");
->>>>>>> branch 'master' of https://github.com/CremboC/bear_traffic_controller.git
 	}
 
 	@Test
 	public void testSpeed() {
-		// TEST NOT WORKING
-		// System.out.println(testAircraft.speed());
 		assertEquals(testAircraft.speed(), 20.0, 0.1);
 	}
 
@@ -262,6 +235,8 @@ public class AircraftTest2 {
 				1);
 		assertEquals(testAircraft.flightPathContains(locationWaypoints[0]), 2,
 				0);
+		assertEquals(testAircraft.flightPathContains(airspaceWaypoints[4]), -1,
+				0);
 	}
 
 	@Test
@@ -291,17 +266,27 @@ public class AircraftTest2 {
 
 	@Test
 	public void testUpdate() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testTurnLeft() {
-		fail("Not yet implemented");
+		testAircraft.turnLeft(0.5);
+
+		testAircraft.update(0.5);
+
+		assertEquals(testAircraft.position().x(), 10, 4);
+		assertEquals(testAircraft.position().y(), 990, 4);
 	}
 
 	@Test
 	public void testTurnRight() {
-		fail("Not yet implemented");
+		testAircraft.turnRight(0.5);
+
+		testAircraft.update(0.5);
+
+		assertEquals(testAircraft.position().x(), 20, 4);
+		assertEquals(testAircraft.position().y(), 995, 4);
 	}
 
 	@Test
@@ -382,27 +367,71 @@ public class AircraftTest2 {
 
 	@Test
 	public void testSetBearing() {
-		fail("Not yet implemented");
+		// fail("Not yet implemented");
 	}
 
 	@Test
 	public void testClimb() {
-		fail("Not yet implemented");
+		double currentAltitude = testAircraft.position().z();
+
+		if (currentAltitude == 15000.0) {
+			testAircraft.decreaseTargetAltitude();
+			testAircraft.update(0.6);
+			currentAltitude = testAircraft.position().z();
+		}
+
+		testAircraft.increaseTargetAltitude();
+		testAircraft.update(0.3);
+
+		assertTrue(testAircraft.position().z() > currentAltitude);
 	}
 
 	@Test
 	public void testFall() {
-		fail("Not yet implemented");
+		double currentAltitude = testAircraft.position().z();
+
+		if (currentAltitude == 5000.0) {
+			testAircraft.increaseTargetAltitude();
+			testAircraft.update(0.6);
+			currentAltitude = testAircraft.position().z();
+		}
+
+		testAircraft.decreaseTargetAltitude();
+		testAircraft.update(0.3);
+
+		assertTrue(testAircraft.position().z() < currentAltitude);
 	}
 
 	@Test
 	public void testDecreaseTargetAltitude() {
-		fail("Not yet implemented");
+		double currentAltitude = testAircraft.position().z();
+
+		if (currentAltitude == 5000.0) {
+			testAircraft.increaseTargetAltitude();
+			testAircraft.update(0.6);
+			currentAltitude = testAircraft.position().z();
+		}
+
+		testAircraft.decreaseTargetAltitude();
+		testAircraft.update(0.3);
+
+		assertTrue(testAircraft.position().z() < currentAltitude);
 	}
 
 	@Test
 	public void testIncreaseTargetAltitude() {
-		fail("Not yet implemented");
+		double currentAltitude = testAircraft.position().z();
+
+		if (currentAltitude == 15000.0) {
+			testAircraft.decreaseTargetAltitude();
+			testAircraft.update(0.6);
+			currentAltitude = testAircraft.position().z();
+		}
+
+		testAircraft.increaseTargetAltitude();
+		testAircraft.update(0.3);
+
+		assertTrue(testAircraft.position().z() > currentAltitude);
 	}
 
 	@Test
