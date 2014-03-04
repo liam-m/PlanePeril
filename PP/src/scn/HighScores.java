4ab52ab84f5ba3cd3fe2cf4a42af5cd9256f000e
Liam Mullane <lm987@york.ac.uk>
2014-03-04 14:42:55 +0000

    Refactor HighScores

--- PP/src/scn/HighScores.java
+++ PP/src/scn/HighScores.java
@@ -13,62 +13,47 @@ import lib.jog.graphics;
 import lib.jog.window;
 
 public class HighScores extends Scene {
-
-	// Maximum number of scores to display on screen; if amount of scores stored
-	// is lower, uses that.
-	private static final int SCORES_TO_DISPLAY = 10;
-
-	// Text box to write the details of the game failure
-	private TextBox textBox;
+	private static final int MAX_NUM_SCORES_TO_DISPLAY = 10;
+	private TextBox text_box; // Text box to write the details of the game failure
 
 	public HighScores(Main main) {
 		super(main);
 	}
 
 	/**
-	 * loads the high scores screen including deserializing the scores file and
+	 * Loads the high scores screen including deserializing the scores file and
 	 * displaying it on the screen sorted.
 	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void start() {
-
 		ArrayList<Integer> scores = new ArrayList<Integer>();
 
-		// if file exists and is deserializeable
-		if (Serializer.deserialize(Main.SCORES_FILE)) {
+		if (Serializer.deserialize(Main.SCORES_FILE)) { // If file exists and is deserializeable
 			scores = (ArrayList<Integer>) Serializer.getRecovered();
 		}
 
-		textBox = new lib.TextBox(64, 96, window.width() - 128,
-				window.height() - 96, 32);
-		textBox.addText("HIGH SCORES");
+		text_box = new lib.TextBox(64, 96, window.width() - 128, window.height() - 96, 32);
+		text_box.addText("HIGH SCORES");
 
-		// convert arraylist into regular array to make sorting simpler
+		// Convert ArrayList into regular array to make sorting simpler
 		Integer[] scoresArray = scores.toArray(new Integer[scores.size()]);
 		Arrays.sort(scoresArray, Collections.reverseOrder());
 		
-		int iMax = SCORES_TO_DISPLAY; 
-		
-		// if there are less scores than max, display all of them
-		if (scores.size() < SCORES_TO_DISPLAY)
-			iMax = scores.size() - 1;
-
-		// Display the 'SCORES_TO_DISPLAY' highest scores.
-		for (int i = 1; i <= iMax; i++) {
-			textBox.delay(0.2);
-			textBox.addText(i + ": " + scoresArray[i]);
+		int num_scores_to_display = scores.size() < MAX_NUM_SCORES_TO_DISPLAY ? scores.size()-1 : MAX_NUM_SCORES_TO_DISPLAY; 
+		for (int i = 1; i <= num_scores_to_display; i++) {
+			text_box.delay(0.2);
+			text_box.addText(i + ": " + scoresArray[i]);
 		}
-
 	}
 
-	@Override
 	/**
 	 * If before explosion has finished, update the explosion
 	 * otherwise, update text box instead
 	 */
-	public void update(double dt) {
-		textBox.update(dt);
+	@Override
+	public void update(double time_difference) {
+		text_box.update(time_difference);
 	}
 
 	@Override
@@ -79,11 +64,11 @@ public class HighScores extends Scene {
 	public void mouseReleased(int key, int x, int y) {
 	}
 
-	@Override
 	/**
 	 * Tracks if any keys are pressed when the game over screen begins
 	 * Prevents the scene instantly ending due to a key press from previous scene
 	 */
+	@Override
 	public void keyPressed(int key) {
 	}
 
@@ -101,11 +86,10 @@ public class HighScores extends Scene {
 	public void draw() {
 		graphics.setColour(Main.GREEN);
 
-		textBox.draw();
+		text_box.draw();
 
 		graphics.setColour(0, 128, 0, 255);
-		graphics.printCentred("Press any key to continue", 0,
-				window.height() - 256, 1, window.width());
+		graphics.printCentred("Press any key to continue", 0, window.height() - 256, 1, window.width());
 	}
 
 	@Override
@@ -116,5 +100,4 @@ public class HighScores extends Scene {
 	public void playSound(Sound sound) {
 
 	}
-
 }
