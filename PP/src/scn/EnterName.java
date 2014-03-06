678aa041cd4f895608ba892c11b86d9b9acb1b7e
Liam Mullane <lm987@york.ac.uk>
2014-03-06 11:53:24 +0000

    EnterName

---
+++ PP/src/scn/EnterName.java
@@ -0,0 +1,47 @@
+package scn;
+
+import pp.Main;
+import lib.jog.audio.Sound;
+
+public class EnterName extends Scene {
+
+	public EnterName(Main main) {
+		super(main);
+	}
+
+	@Override
+	public void mousePressed(int key, int x, int y) {
+	}
+
+	@Override
+	public void mouseReleased(int key, int x, int y) {
+	}
+
+	@Override
+	public void keyPressed(int key) {
+	}
+
+	@Override
+	public void keyReleased(int key) {
+	}
+
+	@Override
+	public void start() {
+	}
+
+	@Override
+	public void update(double time_difference) {
+	}
+
+	@Override
+	public void draw() {
+	}
+
+	@Override
+	public void close() {
+	}
+
+	@Override
+	public void playSound(Sound sound) {
+	}
+}
