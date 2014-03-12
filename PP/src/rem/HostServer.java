425865bf930e351859d834e9adf86abec03597c0
Liam Mullane <lm987@york.ac.uk>
2014-03-12 16:03:23 +0000

    Remote package

---
+++ PP/src/rem/HostServer.java
@@ -0,0 +1,40 @@
+package rem;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+
+import scn.Host;
+
+public class HostServer extends UnicastRemoteObject implements HostInterface {
+	Registry registry; 
+	int port = 1729;
+	Host host;
+	String my_name, address, their_name;
+	
+	public HostServer(Host host) throws RemoteException {
+		super();
+		registry = LocateRegistry.createRegistry(port);
+		registry.rebind("host_server", this);
+		this.host = host;
+		this.my_name = host.getPlayerName();
+	}
+	
+	public String getMyName() {
+		return this.my_name;
+	}
+	
+	public String getTheirName() {
+		return this.their_name;
+	}
+
+	@Override
+	public String connect(String address, String their_name) throws RemoteException {
+		host.connected(address, their_name);
+		this.address = address;
+		this.their_name = their_name;
+		return my_name;
+	}	
+}
---
+++ PP/src/rem/JoinClient.java
@@ -0,0 +1,24 @@
+package rem;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+
+public class JoinClient {
+	int server_port = 1729;
+	
+	public String connect(String server_address, String my_address, String my_name) throws RemoteException {
+		HostInterface host_interface;
+		Registry registry;
+		try {
+			registry = LocateRegistry.getRegistry(server_address, server_port);
+			host_interface = (HostInterface)(registry.lookup("host_server"));
+			return host_interface.connect(my_address, my_name);
+		} catch (NotBoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
