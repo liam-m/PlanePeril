f2556591b549a03f0261088ae25a8a3d6fe3d21c
Liam Mullane <lm987@york.ac.uk>
2014-03-13 11:27:32 +0000

    Connect on different ports

--- PP/src/rem/MultiplayerServer.java
+++ PP/src/rem/MultiplayerServer.java
@@ -7,22 +7,23 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class MultiplayerServer extends UnicastRemoteObject implements MultiplayerInterface {
-	int server_port = 1730;
 	String opponent_address;
 	Registry registry;
 	MultiplayerInterface multiplayer_interface;
 	Registry their_registry;
+	int opponent_port;
 
-	public MultiplayerServer(String opponent_address, String registry_name) throws RemoteException {
+	public MultiplayerServer(String opponent_address, String registry_name, int my_port, int opponent_port) throws RemoteException {
 		super();
-		registry = LocateRegistry.createRegistry(server_port);
+		registry = LocateRegistry.createRegistry(my_port);
 		registry.rebind(registry_name, this);
 		this.opponent_address = opponent_address;
+		this.opponent_port = opponent_port;
 	}
 	
 	public void connect(String registry_name) {
 		try {
-			their_registry = LocateRegistry.getRegistry(opponent_address, server_port);
+			their_registry = LocateRegistry.getRegistry(opponent_address, opponent_port);
 			multiplayer_interface = (MultiplayerInterface)(their_registry.lookup(registry_name));
 		} catch (NotBoundException | RemoteException e) {
 			e.printStackTrace();
