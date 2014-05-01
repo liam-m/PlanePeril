fb647b00d2c60534d56b9ae4b53ec5760ef9d01a
Liam Wellacott <liamwellacott@yahoo.co.uk>
2014-05-01 15:32:27 +0100

    added buttons to game over instead of pressing any key

--- PlanePeril/src/scn/GameOverMult.java
+++ PlanePeril/src/scn/GameOverMult.java
@@ -3,6 +3,8 @@ package scn;
 import java.awt.Window;
 
 import pp.Main;
+import lib.ButtonText;
+import lib.ButtonText.Action;
 import lib.jog.audio.Sound;
 import lib.jog.graphics;
 import lib.jog.input;
@@ -10,10 +12,32 @@ import lib.jog.window;
 
 public class GameOverMult extends Scene {
 	boolean win;
+	ButtonText play_again, exit;
 	
-	public GameOverMult(Main main, boolean win) {
+	
+	// 	public ButtonText(String text, Action action, int x_coordinate, int y_coordinate, int width, int height, int x_offset, int y_offset, boolean border, boolean centred) {
+
+	public GameOverMult(final Main main, boolean win) {
 		super(main);
 		this.win = win;
+		
+		ButtonText.Action replay = new ButtonText.Action() {
+			@Override
+			public void action() {
+				main.closeScene();
+				main.setScene(new EnterName(main));
+			}
+		};
+		play_again = new ButtonText("play again", replay, window.getWidth()/2 -100, window.getHeight()/2, 100, 30, true, true);
+		
+		ButtonText.Action exit_action = new ButtonText.Action() {
+			@Override
+			public void action() {
+				main.closeScene();
+			}
+		};
+		exit = new ButtonText("exit", exit_action, window.getWidth()/2 +100, window.getHeight()/2, 100, 30, true, true);
+		
 	}
 	
 	@Override
@@ -21,12 +45,20 @@ public class GameOverMult extends Scene {
 		graphics.setColour(Main.GREEN);
 		String text = win ? "You Win!" : "You Lose!";
 		graphics.printTextCentred(text, window.getWidth()/4, 200, 20, 750);
-		graphics.printTextCentred("Press any key to return to the main menu", window.getWidth()/2 , window.getHeight() - 200, 3, 200);
+		graphics.printTextCentred("Press a button to play again or exit", window.getWidth()/2 , window.getHeight() - 200, 3, 200);
+		play_again.draw();
+		exit.draw();
 
 	}
 	@Override
 	public void mousePressed(int key, int mouse_x, int mouse_y) {
-		// TODO Auto-generated method stub
+		if (key == input.MOUSE_LEFT) {
+			if (play_again.isMouseOver(mouse_x, mouse_y)) {
+				play_again.act();
+			} else if (exit.isMouseOver(mouse_x, mouse_y)){
+				exit.act();
+			}
+		}
 
 	}
 
@@ -44,7 +76,7 @@ public class GameOverMult extends Scene {
 
 	@Override
 	public void keyReleased(int key) {
-		main.closeScene();
+
 	}
 
 	@Override
